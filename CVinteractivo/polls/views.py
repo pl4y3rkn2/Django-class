effd8de4db7afdd821a3925c26d9de6b9747bc0e
pl4y3rkn2 <kevinechenique18@gmail.com>
2022-08-31 06:01:20 -0400

    Creando la vista vote, mas todas las clases completas

--- CVinteractivo/polls/views.py
+++ CVinteractivo/polls/views.py
@@ -1,5 +1,39 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+
+from .models import Choice, Question
+
 
 def index(request):
-    return HttpResponse("Hello World")
+    latest_question_list = Question.objects.all()
+    return render(request, "polls/index.html", {
+        "latest_question_list": latest_question_list
+    })
+
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/detail.html", {
+        "question": question
+    })
+
+
+def results(request, question_id):
+    return HttpResponse(f"Estas viendo los resultado de la pregunta nùmero {question_id}")
+
+
+def vote(request, question_id):
+    question =  get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST["choice"])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, "polls/detail.html", {
+            "question": question,
+            "error_message": "No elegiste un respuesta"
+        }
+        )
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse("polls:results", args=(question.id)))
